ccf87582f2a3856e323804b87d826b24735cb79c
sock_magic <startxingcheng@gmail.com>
2024-04-20 10:12:33 +0800

    refactor: 添加子模块及修改模块配置

--- okdownload-connection-okhttp/build.gradle.kts
+++ okdownload-connection-okhttp/build.gradle.kts
@@ -1,35 +1,10 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.bilibilias.android.library)
+    alias(libs.plugins.bilibilias.android.library.jacoco)
 }
 
 android {
     namespace = "com.liulishuo.okdownload"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
@@ -37,6 +12,4 @@ dependencies {
     implementation("androidx.annotation:annotation:1.7.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
